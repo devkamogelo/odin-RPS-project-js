915185583996c346472884e2a4678a3a981230ac
devkamogelo <99497643+devkamogelo@users.noreply.github.com.>
2025-10-19 12:19:36 +0200

    Create the playRound and playGame functions
    The player can now play multiple rounds and is given a score

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
-var humanScore = 0
-var computerScore = 0
-
 
 function getComputerChoice() {
     var randomNum = Math.random();
     if (randomNum >= 0 && randomNum <= 0.3) {
-        return 'Rock'
+        return 'rock'
     } else if (randomNum >= 0.3 && randomNum <= 0.6){
-        return "Paper"
+        return "paper"
     } else {
-        return "Scissors"
+        return "scissors"
     }
 }
 
 function getHumanChoice() {
-    var choice = prompt("Pick a choice: 1.Rock , 2.Paper , 3.Scissors")
-    if (choice == 1) {
-        return "Rock"
-    } else if (choice == 2){ 
-        return "Paper"
+    var choice = prompt("Pick a choice: Rock , Paper , Scissors")
+    return choice.toLowerCase()
+}
+
+var humanScore = 0
+var computerScore = 0
+var draws = 0
+
+function playRound(computerChoice, humanChoice){
+    if(computerChoice == "rock" && humanChoice == "scissors") {
+        computerScore++
+    } else if(computerChoice == "scissors" && humanChoice == "paper") {
+        computerScore++
+    } else if(computerChoice == "paper" && humanChoice == "rock"){
+        computerScore++
+    } else if (computerChoice == humanChoice){
+        draws++
     } else {
-        return "Scissors"
+        humanScore++
     }
 }
 
+function playGame(){
+    var gamesPlayed = 0;
+    
+    while (gamesPlayed <= 5){
+        playRound(getComputerChoice(), getHumanChoice())
+    }
+
+    if (computerScore > humanScore){
+        console.log("Computer wins. " + computerScore + " : " + humanScore)
+        console.log(draws + " draws.")
+    } else {
+        console.log("You win! " + humanScore + " : " + computerScore);
+    }
+}
+
+playGame()
