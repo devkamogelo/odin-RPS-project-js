2cda7b89aa85fcd4f8f681b21b80d3b2d78986e1
devkamogelo <99497643+devkamogelo@users.noreply.github.com.>
2025-10-20 20:38:15 +0200

    added gameOver() fuction, refactored playRound() function
    this will give the user the ability to play on the browser and not the console, and it will show a final message after 5 rounds.

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const resultHeading = document.querySelector("#winner");
+const paragraph = document.querySelector("#shortMessage");
+const currentScoreDisplay = document.querySelector("#score");
+
+if(!resultHeading){
+    console.error("#winner not found!");
+}
 
 function getComputerChoice() {
     var randomNum = Math.random();
@@ -20,37 +27,65 @@ function getHumanChoice() {
 var humanScore = 0
 var computerScore = 0
 var draws = 0
+var gamesPlayed = 0;
 
 function playRound(computerChoice, humanChoice){
     if(computerChoice == "rock" && humanChoice == "scissors") {
         computerScore++
+        displayResults("Computer Wins!", humanChoice, computerChoice);
     } else if(computerChoice == "scissors" && humanChoice == "paper") {
         computerScore++
+        displayResults("Computer Wins!", humanChoice, computerChoice);
     } else if(computerChoice == "paper" && humanChoice == "rock"){
         computerScore++
+        displayResults("Computer Wins!", humanChoice, computerChoice);
     } else if (computerChoice == humanChoice){
         draws++
+        displayResults("Draw!", humanChoice, computerChoice);
     } else {
         humanScore++
+        displayResults("You Win!", humanChoice, computerChoice);
     }
 }
 
+function displayResults(winner, humanChoice, computerChoice){
+    resultHeading.textContent = winner;
+    paragraph.textContent = "Your Choice: " + humanChoice + ", Computer Choice: " + computerChoice;
+    currentScoreDisplay.textContent = "Current score " + humanScore + " : " + computerScore;
+}
 
-// function playGame(){
-//     var gamesPlayed = 1;
-    
-//     while (gamesPlayed <= 5){
-//         playRound(getComputerChoice(), getHumanChoice())
-//         gamesPlayed++
-//     }
+function gameOver(){
+    resultHeading.textContent = computerScore > humanScore ? "Computer Wins" : "You win";
+    paragraph.textContent = computerScore > humanScore ? "Good luck next time" : "Well done!";
+    currentScoreDisplay.textContent = "Score " + humanScore + " : " + computerScore;
+}
+    const rockButton = document.querySelector("#rock");
+    const paperButton = document.querySelector("#paper");
+    const scissorsButton = document.querySelector("#scissors");
+   
+    rockButton.onclick = () => {
+        if(gamesPlayed < 5){
+            playRound(getComputerChoice(), "rock"); 
+            gamesPlayed++;
+        } else {
+            gameOver()
+        }
+    };
+    paperButton.onclick = () => {
+        if(gamesPlayed < 5){
+            playRound(getComputerChoice(), "paper"); 
+            gamesPlayed++;
+        } else {
+            gameOver()
+        }
+    };
+    scissorsButton.onclick = () => {
+        if(gamesPlayed < 5){
+            playRound(getComputerChoice(), "scissors"); 
+            gamesPlayed++;
+        } else {
+            gameOver()
+        }
+    };
 
-//     if (computerScore > humanScore){
-//         console.log("Computer wins. " + computerScore + " : " + humanScore)
-//         console.log(draws + " draws.")
-//     } else {
-//         console.log("You win! " + humanScore + " : " + computerScore);
-//         console.log(draws + " draws.")
-//     }
-// }
 
-playGame()
